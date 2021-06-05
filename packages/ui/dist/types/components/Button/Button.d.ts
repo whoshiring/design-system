fa7e48f9bf75f1bda041341fca89db0901ab9c9c
George Bockari <bitwhys@Georges-Mac-mini.local>
2021-06-05 03:56:22 -0700

    chore: build library before push...

---
+++ packages/ui/dist/types/components/Button/Button.d.ts
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+export interface ButtonProps {
+    primary?: boolean;
+    borderColor?: string;
+}
+declare const Button: FC<ButtonProps>;
+export default Button;
---
+++ packages/ui/dist/types/components/Button/Button.stories.d.ts
@@ -0,0 +1,6 @@
+import { ButtonProps } from './Button';
+import { Meta, Story } from '@storybook/react';
+export declare const Primary: Story<ButtonProps>;
+export declare const Secondary: Story<ButtonProps>;
+declare const _default: Meta<import("@storybook/react").Args>;
+export default _default;
